4c4bd243ac5455f056db001a8ad1c4a08cb20ddf
firdiwek <frdusisay@gmail.com>
2024-08-01 10:38:07 +0300

    Restructure

--- obj/Debug/net8.0/RpMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/RpMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RpMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cc073c48bdfdb221559aa0e9568199b370f0ea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RpMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RpMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
